e1f2c7feb3245ea0f63a28ff311f77b9f7f4414d
Mikhail Prozorov <mikepro@alfamail.ru>
2021-05-17 19:41:54 +0300

    lesson2.sql update

--- lesson2.sql
+++ lesson2.sql
@@ -1,23 +1,23 @@
 CREATE TABLE Gengres (
-id SERIAL NOT NULL,
+id SERIAL PRIMARY KEY,
 Name varchar(255)
 );
 
 CREATE TABLE Performers (
-id SERIAL NOT NULL,
+id SERIAL PRIMARY KEY,
 genreid int NOT NULL,
 Name varchar(255)
 );
 
 CREATE TABLE Albums (
-id SERIAL NOT NULL,
+id SERIAL PRIMARY KEY,
 performerid int NOT NULL,
 Title varchar(255),
 Year int
 );
 
 CREATE TABLE Tracks (
-id SERIAL NOT NULL,
+id SERIAL PRIMARY KEY,
 albumid int NOT NULL,
 Tite varchar(255),
 Duration int
