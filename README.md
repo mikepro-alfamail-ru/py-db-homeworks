481f1c9e503f02ed8bd390721c715cfdeb5f8d85
Mikhail Prozorov <mikepro@alfamail.ru>
2021-05-18 17:12:40 +0300

    Lesson 3 SQL

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # SQLPY-8 Homeworks by Mikhail Prozorov
+
 ## Lesson 1
+
 [PNG picture](lesson1.png)
+
 ## Lesson 2
+
 [SQL file](lesson2.sql)
+
 ## Lesson 3
+
 [PNG picture](lesson3.png)
-[SQL file](lesson2.sql)
+
+[SQL file](lesson3.sql)
---
+++ lesson3.sql
@@ -0,0 +1,46 @@
+CREATE TABLE Collections (
+id SERIAL PRIMARY KEY,
+Title varchar(255),
+Year integer
+);
+
+CREATE TABLE Gengres (
+id SERIAL PRIMARY KEY,
+Name varchar(50)
+);
+
+CREATE TABLE Performers (
+id SERIAL PRIMARY KEY,
+Name varchar(100)
+);
+
+CREATE TABLE Albums (
+id SERIAL PRIMARY KEY,
+Title varchar(255),
+Year integer
+);
+
+CREATE TABLE Tracks (
+id SERIAL PRIMARY KEY,
+albumid integer REFERENCES Albums(id),
+Tite varchar(255),
+Duration integer
+);
+
+CREATE TABLE PerformersGengres (
+GengreID integer REFERENCES Gengres(id),
+performerID integer REFERENCES Performers(id),
+constraint pkPerformersGengres primary key (GengreID, performerID)
+);
+
+CREATE TABLE PerformersAlbums (
+performerID integer REFERENCES Performers(id),
+albumID integer REFERENCES Albums(id),
+constraint pkPerformersAlbums primary key (performerID, albumID)
+);
+
+CREATE TABLE CollectionsTracks (
+CollectionID integer REFERENCES Collections(id),
+TrackID integer REFERENCES Tracks(id),
+constraint pkCollectionsTracks primary key (CollectionID, TrackID)
+);
