41d155ed5734052f84c2a15ec88df8e3f5421ffb
Mikhail Prozorov <mikepro@alfamail.ru>
2021-05-19 10:51:23 +0300

    Lesson 3 advanced task

---
+++ lesson3_adv.sql
@@ -0,0 +1,20 @@
+﻿create table Departments(
+	id serial primary key,
+	Name varchar(40),
+	location varchar(40)
+);
+
+create table Positions(
+	id serial primary key,
+	Name varchar(40)
+);
+
+create table Employees(
+	id serial primary key,
+	FirstName varchar(40),
+	LastName varchar(40),
+	Phone varchar(20),
+	DepartmentID integer references Departments,
+	PositionID integer references Positions,
+	Supervisor integer 	references Employees
+);
